99d78f46cec786c132d51e5c76d3f4ce19b42f3b
Bartlomiej Kedziora <hattifnattar.kodama@gmail.com>
2018-02-08 09:39:45 +0100

    add christmas tree solution from task 1

---
+++ Java/Exercises/samouczekprogramisty/ChristmasTree/Learn.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package learn;
+
+import java.util.*;
+
+/**
+ *
+ * @author Bartlomiej_Kedziora
+ */
+public class Learn {
+
+  /**
+   * @param args the command line arguments
+   */
+  public static void main(String[] args) {
+    
+      ChristmasTree tree = new ChristmasTree(3);
+      System.out.println(tree.build());
+   
+  }
+}
